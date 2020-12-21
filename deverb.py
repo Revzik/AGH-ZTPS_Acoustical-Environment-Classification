f1d1bed47a31c182c8e3686fcda9f63af2fc80e0
revzik <b.piekarz1997@gmail.com>
2020-12-21 18:26:48 +0100

    Add loading of sample impulse response

--- deverb.py
+++ deverb.py
@@ -13,19 +13,41 @@ Copyright to:
 """
 import numpy as np
 import scipy as sp
+import pickle
+
+from read_impulse_response import compute_stft
+from parameterization import STFT
 
 
-def dereverberate(wave, fs):
+def dereverberate(wave, fs, expected_response_path="real_impulse_responses/rir_medium.p", preloaded=True):
     """
     Estimates the impulse response in a room the recording took place
 
     :param wave: 1-D ndarray of wave samples
     :param fs: sampling frequency (int or float)
-    :returns: 1-D ndarray of the impulse response
-              1-D ndarray of the dry signal
-              1-D ndarray of the wet signal
+    :param expected_response_path: path to a file containing expected impulse response (default: rir_medium.p)
+    :param preloaded: is the response preloaded (default: True)
+    :returns: (h_rir) 1-D ndarray of the impulse response,
+              (wave_dry) 1-D ndarray of the dry signal,
+              (wave_wet) 1-D ndarray of the wet signal
     """
-    h_rir = np.zeros(wave.shape)
+    if preloaded:
+        with open(expected_response_path, "rb") as f:
+            ref_imp = pickle.load(f)
+
+        win_len = ref_imp["window_length"]
+        ovlap = ref_imp["overlap"]
+        nfft = ref_imp["nfft"]
+        blocks = ref_imp["blocks"]
+        imp = ref_imp["stft"]
+    else:
+        win_len = 1024
+        ovlap = 512
+        nfft = 1024
+        blocks = 400
+        imp = compute_stft(expected_response_path, win_len, ovlap, nfft, blocks)
+
+    h_rir = np.zeros(blocks)
     wave_dry = np.zeros(wave.shape)
     wave_wet = np.zeros(wave.shape)
 
---
+++ read_impulse_response.py
@@ -0,0 +1,75 @@
+import pickle
+import soundfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+from parameterization import STFT
+
+
+def compute_stft(path, window_length, overlap, nfft, blocks):
+    """
+    Computes stft of an reference impulse response used to estimate the one from a recording.
+
+    :param path: path to a soundfile (wav)
+    :param window_length: length of a window (samples)
+    :param overlap: window overlap (samples)
+    :param nfft: number of frequency bins
+    :param blocks: number of output frequency blocks. If STFT is shorter, then it will get padded with zeros
+    :return: stft of an impulse response
+    """
+    window = np.hanning(window_length)
+    wave, fs = soundfile.read(path)
+
+    tmp_stft = STFT(wave, window, overlap, nfft)
+
+    K = tmp_stft.shape[0]
+    stft = np.zeros((blocks, nfft))
+    if K >= blocks:
+        stft = tmp_stft[0:blocks, :]
+    else:
+        stft[0:K, :] = tmp_stft
+
+    return stft
+
+
+def save(path, stft, window_length, overlap, nfft, blocks):
+    """
+    Saves the response into a file
+
+    :param path: path to save the file to
+    :param stft: stft of an impulse response wave
+    :param window_length: window_length used
+    :param overlap: overlap used
+    :param nfft: number of frequency bins used
+    :param blocks: number of stft blocks
+    """
+    impulse_dict = {
+        "window_length": window_length,
+        "overlap": overlap,
+        "nfft": nfft,
+        "blocks": blocks,
+        "stft": stft
+    }
+
+    with open(path, "wb") as f:
+        pickle.dump(impulse_dict, f)
+
+
+if __name__ == "__main__":
+    _path_in = "real_impulse_responses/IMP_bunker_b.wav"
+    _window_length = 1024
+    _overlap = 512
+    _nfft = 1024
+    _blocks = 200
+
+    _path_out = "real_impulse_responses/rir_long.p"
+
+    _stft = compute_stft(_path_in, _window_length, _overlap, _nfft, _blocks)
+    save(_path_out, _stft, _window_length, _overlap, _nfft, _blocks)
+
+    fig, axes = plt.subplots(2, 1, figsize=(8, 6))
+    wave, fs = soundfile.read(_path_in)
+    axes[0].plot(wave)
+    axes[1].pcolormesh(_stft.T)
+    axes[1].set_ylim([0, _stft.shape[1] / 2])
+    fig.show()
