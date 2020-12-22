21aedd2eecaf18f46876889d3325023fdc9b8586
revzik <b.piekarz1997@gmail.com>
2020-12-22 01:05:48 +0100

    Prepare constants and variables for the algorithm

--- deverb.py
+++ deverb.py
@@ -31,21 +31,65 @@ def dereverberate(wave, fs, expected_response_path="real_impulse_responses/rir_m
               (wave_dry) 1-D ndarray of the dry signal,
               (wave_wet) 1-D ndarray of the wet signal
     """
+    # =========== Reference impulse response ===========
     if preloaded:
         with open(expected_response_path, "rb") as f:
             ref_imp = pickle.load(f)
 
         win_len = ref_imp["window_length"]
-        ovlap = ref_imp["overlap"]
+        win_ovlap = ref_imp["overlap"]
         nfft = ref_imp["nfft"]
         blocks = ref_imp["blocks"]
-        imp = ref_imp["stft"]
+        imp_stft = ref_imp["stft"]
     else:
         win_len = 1024
-        ovlap = 512
+        win_ovlap = 512
         nfft = 1024
         blocks = 400
-        imp = compute_stft(expected_response_path, win_len, ovlap, nfft, blocks)
+        imp_stft = compute_stft(expected_response_path, win_len, win_ovlap, nfft, blocks)
+
+    # =================== Signal stft ==================
+    window = np.hanning(win_len)
+    sig_stft = STFT(wave, window, win_ovlap, nfft)
+
+    # ==================== Constants ===================
+    # window step / hop
+    win_step = win_len - win_ovlap
+
+    # minimum gain per frequency
+    min_gain = np.zeros(nfft)
+
+    # maximum impulse response estimate
+    max_h = imp_stft
+    # max_h = np.ones((blocks, nfft)) * 0.9
+
+    # bias used to keep magnitudes from getting stuck on a wrong minimum
+    bias = np.ones((blocks, nfft)) * 1.01
+
+    # alpha and gamma - smoothing factors for impulse response magnitude and gain
+    alpha = np.ones((blocks, nfft)) * 0.2
+    gamma = np.ones(nfft) * 0.3
+
+    # ==================== Algorithm ===================
+    # dry_stft and wet_stft are the estimated dry and reverberant signals in frequency-time domain
+    dry_stft = np.zeros((blocks, nfft))
+    wet_stft = np.zeros((blocks, nfft))
+
+    # h_stft is the estimated impulse response in frequency-time domain
+    h_stft = max_h / 2
+    # h_stft = np.zeros((blocks, nfft))
+
+    # raw_frames and dry_frames are matrices to keep the information of currently estimated raw and dry signal
+    raw_frames = np.ones((blocks, nfft))
+    dry_frames = np.zeros((blocks, nfft))
+
+    # C is a matrix to keep the raw estimated powers of the impulse response
+    c = np.zeros((blocks, nfft))
+
+    # gain_dry and gain_wet are the frequency gains of the dry and wet signals
+    gain_dry = np.ones(nfft)
+    gain_wet = np.zeros(nfft)
+
 
     h_rir = np.zeros(blocks)
     wave_dry = np.zeros(wave.shape)
