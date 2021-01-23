82964792f3c21b173feccb8c3e13d1e97cdffb3c
revzik <b.piekarz1997@gmail.com>
2021-01-23 17:45:00 +0100

    Add fs, frame count and frequency count to files

--- load_test.py
+++ load_test.py
@@ -3,13 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with open("out/bathroom/bathroom_4.p", "rb") as f:
-    H_rir = pickle.load(f)
+with open("out\\bathroom\\bathroom_4.p", "rb") as f:
+    data = pickle.load(f)
+    H_rir = data["h_rir"]
+    fs = data["fs"]
+    frame_count = data["frame_count"]
+    freq_count = data["freq_count"]
 
-frames, freqs = H_rir.shape
 hop = 0.01
-f = np.linspace(0, 44100 / 2000, freqs)
-t = np.linspace(0, hop * frames, frames)
+f = np.linspace(0, fs / 2000, freq_count)
+t = np.linspace(0, hop * frame_count, frame_count)
 fxx, txx = np.meshgrid(f, t)
 
 fig, ax = plt.subplots(figsize=(6, 5))
